a9be407b2b6ac5360213b98d6776bda5f03e5060
maheshogit <mahesh.java258@gmail.com>
2020-09-17 17:23:22 +0530

    jenkins hellow commit

---
+++ src/main/java/com/jenkins/jenkinsAmm/JenkinsAmmApplication.java
@@ -0,0 +1,13 @@
+package com.jenkins.jenkinsAmm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JenkinsAmmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JenkinsAmmApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/jenkins/jenkinsAmm/controller/RestController.java
@@ -0,0 +1,12 @@
+package com.jenkins.jenkinsAmm.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+@org.springframework.web.bind.annotation.RestController
+public class RestController {
+
+    @GetMapping("/jen")
+    public String getDemo(){
+        return "Hellow Jenknins";
+    }
+}
